eb3d628ce7e8d9adb909b042a3a359d5ffba63b4
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-21 14:06:18 -0700

    Refactor code to randomly generate 3 out of 13 possible questions
    co-authored-by: Mike Manchee <mikemanchee@gmail.com>

--- Riddles/Program.cs
+++ Riddles/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Riddle.Answer;
 
 namespace Riddle {
@@ -8,58 +9,34 @@ namespace Riddle {
     public static void Main()
     {
       Phinx outPut = new Phinx();
- 
-      Console.WriteLine("Can you defeat the Sphinx? If you answer the riddle correctly, you win. Otherwise, you'll be eaten.");
-      Console.WriteLine("Question 1: What month of the year has 28 days?");
-      string userAnswerOne = Console.ReadLine();
 
-      bool result = outPut.CheckAnswerOne(userAnswerOne);
-      if ( result )
-      {
-        Console.WriteLine("Nice work, now for a second question.");
-      }
-      else
-      {
-        Console.WriteLine("You've been eaten by the Phinx.");
-      }
+      var random = new Random();
 
-      Console.WriteLine("Question 2: What can run, but never walks; has a mouth, but never talks; has a head, but never weeps; has a bed, but never sleeps?");
-      string userAnswerTwo = Console.ReadLine();
+      List<int> qaList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
 
-      bool resultTwo = outPut.CheckAnswerTwo(userAnswerTwo);
-      if ( resultTwo )
+      for (int index = 1; index < 4; index++)
       {
-        Console.WriteLine("You win.");
-      }
-      else
-      {
-        Console.WriteLine("You've been eaten by the Phinx.");
+        int ran = random.Next(qaList.Count);        // randomizer
+        int num = qaList[ran - 1];
+        qaList.Remove(num);                         // take away asked question
+        string question = outPut.GetQuestion(num);  // send for question
+        Console.WriteLine(index + ". " + question); // write question
+        string userAnswer = Console.ReadLine();      // get answer
+        bool result = outPut.CheckAnswer(num, userAnswer);    // send answer to check if correct
+        if (result)                                   // if true ask next question or win and break
+        {
+          if ( index == 3 )
+          {
+            Console.WriteLine("You win the game.");
+            break;
+          }
+        }
+        else                                          // else false, user eaten and break.
+        {
+          Console.WriteLine("You have been eaten by the Sphinx.");
+          break;
+        }
       }
     }
   }
 }
-
-    // Riddle: This thing all things devour: birds, beasts, trees, flowers; gnaws iron, bites steel; grinds hard stones to meal; slays kings, ruins towns; and beats high mountains down.
-    // Answer: Time
-    // Riddle: In spring I am gay in handsome array; in summer more clothing I wear; when colder it grows, I fling off my clothes; and in winter quite naked appear.
-    // Answer: A tree
-    // Riddle: I’m alive, but without breath; I’m as cold in life as in death; I’m never thirsty, though I always drink.
-    // Answer: A fish
-    // Riddle: As small as your thumb, I am light in the air. You may hear me before you see me, but trust that I’m here.
-    // Answer: A hummingbird
-    // Riddle: Never resting, never still; moving silently from hill to hill; it does not walk, run or trot; all is cool where it is not.
-    // Answer: The sun
-    // Riddle: What can bring back the dead; make you cry, make you laugh, make you young; is born in an instant, yet lasts a lifetime.
-    // Answer: Memory
-    // Riddle: What is always old and sometimes new; never sad, sometimes blue; never empty, but sometimes full; never pushes, always pulls?
-    // Answer: The moon
-    // Riddle: What is large, yet never grows; has roots that cannot be seen; and is taller than trees?
-    // Answer: A mountain
-    // Riddle: At night they come without being fetched, and by day they are lost without being stolen.
-    // Answer: Stars
-    // Riddle: I create my lair with earthen string, and dispatch my prey with a biting sting.
-    // Answer: A spider
-    // Riddle: Some try to hide, some try to cheat; but time will show, we always will meet. Try as you might to guess my name.
-    // Answer: Death
-    // Riddle: What can run, but never walks; has a mouth, but never talks; has a head, but never weeps; has a bed, but never sleeps?
-    // Answer: A river
--- Riddles/Riddles.cs
+++ Riddles/Riddles.cs
@@ -4,47 +4,99 @@ namespace Riddle.Answer {
 
   public class Phinx
   {
-    public bool CheckAnswerOne(string userAnswerOne)
+    public string GetQuestion(int num)
     {
-      if ( userAnswerOne.Contains("all") )
+      switch (num)
       {
-        return true;
-      }
-      else
-      {
-        switch (userAnswerOne.ToLower())
-        {
-          case "january":
-          case "february":
-          case "march":
-          case "april":
-          case "may":
-          case "june":
-          case "july":
-          case "august":
-          case "september":
-          case "october":
-          case "november":
-          case "december":
-            return true;
-          default:
-            return false;
-        }
+        case 1:
+          return "What month of the year has 28 days?";
+        case 2:
+          return "What can run, but never walks; has a mouth, but never talks; has a head, but never weeps; has a bed, but never sleeps?";
+        case 3:
+          return "This thing all things devour: birds, beasts, trees, flowers; gnaws iron, bites steel; grinds hard stones to meal; slays kings, ruins towns; and beats high mountains down.";
+        case 4:
+          return "In spring I am gay in handsome array; in summer more clothing I wear; when colder it grows, I fling off my clothes; and in winter quite naked appear.";
+        case 5:
+          return "I’m alive, but without breath; I’m as cold in life as in death; I’m never thirsty, though I always drink.";
+        case 6:
+          return "As small as your thumb, I am light in the air. You may hear me before you see me, but trust that I’m here.";
+        case 7:
+          return "Never resting, never still; moving silently from hill to hill; it does not walk, run or trot; all is cool where it is not.";
+        case 8:
+          return "What can bring back the dead; make you cry, make you laugh, make you young; is born in an instant, yet lasts a lifetime.";
+        case 9:
+          return "What is always old and sometimes new; never sad, sometimes blue; never empty, but sometimes full; never pushes, always pulls?";
+        case 10:
+          return "What is large, yet never grows; has roots that cannot be seen; and is taller than trees?";
+        case 11:
+          return "At night they come without being fetched, and by day they are lost without being stolen.";
+        case 12:
+          return "I create my lair with earthen string, and dispatch my prey with a biting sting.";
+        case 13:
+          return "Some try to hide, some try to cheat; but time will show, we always will meet. Try as you might to guess my name.";
+        default:
+          return "false";
       }
     }
-
-    public bool CheckAnswerTwo(string userAnswerTwo)
+    public bool CheckAnswer(int num, string userAnswer)
     {
-      // bool answer = userAnswerTwo.Contains("river") ? true : false;
-      // return answer;
-      if ( userAnswerTwo.Contains("river") )
+      userAnswer.ToLower();
+
+      switch (num)
       {
-        return true;
+        case 1:
+          if ( userAnswer.Contains("all") )
+          {
+            return true;
+          }
+          else
+          {
+            switch (userAnswer)
+            {
+              case "january":
+              case "february":
+              case "march":
+              case "april":
+              case "may":
+              case "june":
+              case "july":
+              case "august":
+              case "september":
+              case "october":
+              case "november":
+              case "december":
+                return true;
+              default:
+                return false;
+            }
+          }
+        case 2:
+          return userAnswer.Contains("river") ? true : false;
+        case 3:
+          return userAnswer.Contains("time") ? true : false;
+        case 4:
+          return userAnswer.Contains("tree") ? true : false;
+        case 5:
+          return userAnswer.Contains("fish") ? true : false;
+        case 6:
+          return userAnswer.Contains("hummingbird") ? true : false;
+        case 7:
+          return userAnswer.Contains("sun") ? true : false;
+        case 8:
+          return userAnswer.Contains("memory") ? true : false;
+        case 9:
+          return userAnswer.Contains("moon") ? true : false;
+        case 10:
+          return userAnswer.Contains("mountain") ? true : false;
+        case 11:
+          return userAnswer.Contains("stars") ? true : false;
+        case 12:
+          return userAnswer.Contains("spider") ? true : false;
+        case 13:
+          return userAnswer.Contains("death") ? true : false;
+        default:
+          return false;
       }
-      else
-      {
-        return false;
-      }
-    }    
+    }   
   }
 }
